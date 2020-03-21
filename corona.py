9ed15f65d2f6c1a80822587660da91848e72565f
Starvexx <david_wieser@hotmail.com>
2020-03-21 23:26:34 +0100

    Initial Upload

---
+++ corona.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python3
+"""
+Author:     David Hernandez
+E-Mail:     david.hernandez@univie.ac.at
+Version:    0.1
+
+This is a little script to analyze the Corona outbreak by passing a datafile
+containing date, number of tests administered, number of infected, number of
+deaths and number of cured/recovered persons.
+    The script then will read the data and create plots so that it can be
+analyzed visually.
+"""
+from matplotlib import pyplot as plt
+from matplotlib import dates as mpldate
+from scipy.optimize import curve_fit
+import numpy as np
+import datetime as dt
+import argparse as ap
+
+def func(x, a, b):
+    return a * np.exp(-b * x)
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser(description="Plot some corona data.")
+    parser.add_argument('--datafile', '-f',
+                        type=str,
+                        help='The datafile that contains the corona data.')
+    args = parser.parse_args()
+
+    data = np.genfromtxt(args.datafile, delimiter=',', skip_header=1)
+
+    days = []
+    tested = []
+    infected = []
+    dead = []
+    recovered = []
+
+    for line in data:
+        days.append(mpldate.date2num(dt.date(int(line[0]),
+                                             int(line[1]),
+                                             int(line[2]))))
+        tested.append(line[3])
+        infected.append(line[4])
+        dead.append(line[5])
+        recovered.append(line[6])
+
+    prospect = np.polyfit(days, infected, 4)
+    days_new = np.arange(0, len(days), 1)
+    popt, pcov = curve_fit(func, days_new, infected)
+#    print(days)
+#    interpolated = []
+    timeline = np.arange(737480.0, 737480.0+30, 0.1)
+    t_new = np.arange(0.0, 30.0, 0.1)
+    fitted = []
+    for t in t_new:
+        fitted.append(func(t, *popt))
+        # fitted.append(prospect[0]*day**4+prospect[1]*day**3+prospect[2]*day**2+prospect[3]*day+prospect[4])
+    print(fitted)
+    fig = plt.figure(figsize=(10, 10))
+    ax1 = fig.add_subplot(311)
+    ax2 = fig.add_subplot(312)
+    ax3 = fig.add_subplot(313)
+    ax1.plot_date(days, infected)
+    ax1.plot(timeline, fitted)
+#    ax1.plot_date(days, func(days, *popt))
+    ax2.plot_date(days, dead, label='Dead')
+    ax2.plot_date(days, recovered, label='Recovered')
+    ax3.plot_date(days, tested)
+
+    ax1.xaxis.set_tick_params(rotation=30, labelsize=10)
+    ax1.grid(True)
+    ax1.set_ylabel('Infected')
+    ax2.xaxis.set_tick_params(rotation=30, labelsize=10)
+    ax2.grid(True)
+    ax2.set_ylabel('Persons')
+    ax3.xaxis.set_tick_params(rotation=30, labelsize=10)
+    ax3.grid(True)
+    ax3.set_xlabel('Datum')
+    ax3.set_ylabel('Persons')
+    plt.show()
