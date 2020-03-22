1d1d0386315945d9f96de829334221d0256c0634
Starvexx <david_wieser@hotmail.com>
2020-03-22 12:48:34 +0100

    minor updates

--- corona.py
+++ corona.py
@@ -18,13 +18,20 @@ import datetime as dt
 import argparse as ap
 
 def func(x, a, b):
-    return a * np.exp(-b * x)
+    """This is the function after which the data is fitted."""
+    return a * np.exp(b * x)
 
 if __name__ == "__main__":
+    #==========================================================================#
+    # Set up the argument parser.
     parser = ap.ArgumentParser(description="Plot some corona data.")
     parser.add_argument('--datafile', '-f',
                         type=str,
                         help='The datafile that contains the corona data.')
+    parser.add_argument('--logaritmic', '-l',
+                        action='store_true',
+                        help='Plot the data on logarithmic scale.',
+                        default=False)
     args = parser.parse_args()
 
     data = np.genfromtxt(args.datafile, delimiter=',', skip_header=1)
@@ -44,27 +51,26 @@ if __name__ == "__main__":
         dead.append(line[5])
         recovered.append(line[6])
 
-    prospect = np.polyfit(days, infected, 4)
     days_new = np.arange(0, len(days), 1)
     popt, pcov = curve_fit(func, days_new, infected)
-#    print(days)
-#    interpolated = []
+
     timeline = np.arange(737480.0, 737480.0+30, 0.1)
     t_new = np.arange(0.0, 30.0, 0.1)
     fitted = []
     for t in t_new:
         fitted.append(func(t, *popt))
-        # fitted.append(prospect[0]*day**4+prospect[1]*day**3+prospect[2]*day**2+prospect[3]*day+prospect[4])
-    print(fitted)
+
     fig = plt.figure(figsize=(10, 10))
     ax1 = fig.add_subplot(311)
     ax2 = fig.add_subplot(312)
     ax3 = fig.add_subplot(313)
-    ax1.plot_date(days, infected)
-    ax1.plot(timeline, fitted)
-#    ax1.plot_date(days, func(days, *popt))
+
+    ax1.plot_date(days, infected, label='Data')
+    ax1.plot(timeline, fitted, label='Exponential fit')
+
     ax2.plot_date(days, dead, label='Dead')
     ax2.plot_date(days, recovered, label='Recovered')
+
     ax3.plot_date(days, tested)
 
     ax1.xaxis.set_tick_params(rotation=30, labelsize=10)
